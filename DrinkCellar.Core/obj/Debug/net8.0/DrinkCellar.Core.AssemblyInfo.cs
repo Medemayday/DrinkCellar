8c04c3d0ef4e7ad743ca86b835224da52e01cd61
Medemayday <matthias.deklerck@student.howest.be>
2024-08-08 18:08:38 +0200

    Update

--- DrinkCellar.Core/obj/Debug/net8.0/DrinkCellar.Core.AssemblyInfo.cs
+++ DrinkCellar.Core/obj/Debug/net8.0/DrinkCellar.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DrinkCellar.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a54661264d479c1090622878aa2add016a2d57c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12ac71a8a6c74e3a8aeefc79e09ad17bbc389f22")]
 [assembly: System.Reflection.AssemblyProductAttribute("DrinkCellar.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DrinkCellar.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
